15039f3f3ff8ddd4e7ff4f25d781cd586e5e736d
mragone <maria.ragone@samaritanministries.org>
2025-05-22 09:32:12 -0400

    fixed login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { LogIn } from 'lucide-react'
-import { useState } from 'react'
+import { LogIn } from "lucide-react";
 import "./App.css";
-import BottomNavBar from './components/BottomNavBar';
 
 function App() {
-  return (
-  <div className="app-container">
-    <LogIn /> 
-  </div>
-  )
+  return <LogIn />;
 }
 
-export default App
+export default App;
